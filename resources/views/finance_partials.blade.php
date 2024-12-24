e1ab324402633d252d3404f92166cd22651c71c1
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-24 16:10:11 -0700

    Add file (plaintext)

---
+++ resources/views/finance_partials.blade.php
@@ -0,0 +1,5 @@
+<!-- Optionally, store partial views or additional snippets for financing here -->
+<div>
+  <h3>Extra Financing Snippets</h3>
+  <p>Use this file for advanced finance calculations or extended UI components.</p>
+</div>
