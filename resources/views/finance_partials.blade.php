98e2e0b3f108f68c00b2b06a95283dcd2c4c08bd
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-24 16:17:18 -0700

    Update from ChatGPT (plaintext)

--- resources/views/finance_partials.blade.php
+++ resources/views/finance_partials.blade.php
@@ -1,5 +1,7 @@
-<!-- Optionally, store partial views or additional snippets for financing here -->
 <div>
   <h3>Extra Financing Snippets</h3>
-  <p>Use this file for advanced finance calculations or extended UI components.</p>
+  <ul>
+    <li>Potential advanced fields for financing (e.g., balloon payments, additional fees, etc.)</li>
+    <li>Tips or disclaimers about financing calculations</li>
+  </ul>
 </div>
