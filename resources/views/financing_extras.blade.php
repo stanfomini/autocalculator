2ea0c2f901c054746f742feb5f0cc2f65d7ce408
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-24 15:46:12 -0700

    Add file (plaintext)

---
+++ resources/views/financing_extras.blade.php
@@ -0,0 +1,3 @@
+<!-- Example additional financing-specific partial or resources that might be used in the future -->
+<h2>Financing Additional Info</h2>
+<p>Use this partial to display extra financing details as needed.</p>
