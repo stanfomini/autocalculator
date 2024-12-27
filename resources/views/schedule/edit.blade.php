be31b82d762028b81484bba01988f6f20f4f64a4
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-27 15:41:46 -0700

    Update from ChatGPT (plaintext)

--- resources/views/schedule/edit.blade.php
+++ resources/views/schedule/edit.blade.php
@@ -2,30 +2,27 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Edit Schedule</title>
+    <title>Edit (Non-SPA)</title>
 </head>
 <body>
-    <h1>Edit (Non-SPA Fallback)</h1>
-    <form method="POST" action="/testing1/{{ $sched->id }}">
+    <h1>Edit</h1>
+    <form action="/testing1/{{ $sched->id }}" method="POST">
         @csrf
         @method('PUT')
         <p>
-            <label>First Name:</label>
-            <input type="text" name="first_name"
-                   value="{{ $sched->first_name }}" required>
+            <label>First Name</label>
+            <input type="text" name="first_name" value="{{ $sched->first_name }}" required>
         </p>
         <p>
-            <label>Last Name:</label>
-            <input type="text" name="last_name"
-                   value="{{ $sched->last_name }}" required>
+            <label>Last Name</label>
+            <input type="text" name="last_name" value="{{ $sched->last_name }}" required>
         </p>
         <p>
-            <label>Phone:</label>
-            <input type="text" name="phone"
-                   value="{{ $sched->phone }}" required>
+            <label>Phone</label>
+            <input type="text" name="phone" value="{{ $sched->phone }}" required>
         </p>
         <p>
-            <label>Datetime:</label>
+            <label>Datetime</label>
             <input type="datetime-local" name="scheduled_at"
                    value="{{ \Carbon\Carbon::parse($sched->scheduled_at)->format('Y-m-d\TH:i') }}"
                    required>
