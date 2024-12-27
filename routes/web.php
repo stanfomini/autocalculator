1444e33278cc59d902ef15b0cfdfc2eefebc823a
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-27 16:11:40 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestItemController;
-use App\Http\Controllers\HelloItemController;
-use App\Http\Controllers\ScheduleController;
-use App\Http\Controllers\ScheduleSseController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\BlogSseController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+use App\Http\Controllers\YesTestController;
+use App\Http\Controllers\YesTestSseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/test', TestItemController::class)->only(['index', 'store']);
-Route::resource('/hello', HelloItemController::class)->only(['index', 'store']);
-
-// The new scheduling resource at /testing1
-Route::resource('/testing1', ScheduleController::class)
-    ->parameters(['testing1' => 'testing1']);
+// Real-time scheduling at /yestest
+Route::resource('/yestest', YesTestController::class)
+    ->parameters(['yestest' => 'yestest']);
 
 // SSE route for real-time
-Route::get('/testing1/sse', [ScheduleSseController::class, 'stream'])->name('testing1.sse');
-
-// Blog Resource
-Route::resource('/blog', BlogController::class);
-// Blog SSE route
-Route::get('/blog/sse', [BlogSseController::class, 'stream'])->name('blog.sse');
+Route::get('/yestest/sse', [YesTestSseController::class, 'stream'])->name('yestest.sse');
