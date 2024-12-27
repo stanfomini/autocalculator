6ec3cf44c2a0c0404e4dbb8ccab66b6f676f3c8b
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-27 15:51:10 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\TestItemController;
 use App\Http\Controllers\HelloItemController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\ScheduleSseController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\BlogSseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +19,9 @@ Route::get('/', function () {
 Route::resource('/test', TestItemController::class)->only(['index', 'store']);
 Route::resource('/hello', HelloItemController::class)->only(['index', 'store']);
 
-// The new scheduling resource at /testing1
+// Schedules resource at /testing1
 Route::resource('/testing1', ScheduleController::class)
     ->parameters(['testing1' => 'testing1']);
 
-// SSE route for real-time
+// SSE endpoint
 Route::get('/testing1/sse', [ScheduleSseController::class, 'stream'])->name('testing1.sse');
-
-// Blog Resource
-Route::resource('/blog', BlogController::class);
-// Blog SSE route
-Route::get('/blog/sse', [BlogSseController::class, 'stream'])->name('blog.sse');
