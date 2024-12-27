156c8e5aca719da8cb61b46de0b28283d006e386
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-27 15:47:26 -0700

    Update from ChatGPT (plaintext)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BlogApiController;
 
 // We can remove the old polling approach for /api/appointments
 // or keep it if desired. Example below is commented out:
@@ -18,3 +19,5 @@ Route::get('/appointments', function () {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/blogs', BlogApiController::class);
