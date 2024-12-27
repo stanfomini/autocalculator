70cba6c224005a8e592666250216d6616cd872f7
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-27 11:19:54 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestItemController;
 use App\Http\Controllers\HelloItemController;
+use App\Http\Controllers\AppointmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,11 @@ Route::resource('/test', TestItemController::class)->only(['index', 'store']);
 // New route for /hello
 Route::resource('/hello', HelloItemController::class)->only(['index', 'store']);
 
+// Dashboard placeholder
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+// New route for /schedule
+Route::get('/schedule', [AppointmentController::class, 'index'])->name('appointments.index');
+Route::post('/schedule', [AppointmentController::class, 'store'])->name('appointments.store');
