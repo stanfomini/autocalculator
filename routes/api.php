bda8deb7043c0d227f8a269835e953cc4c621f4e
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 15:28:55 -0700

    Update from ChatGPT (plaintext)

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\BlogApiController;
+use App\Http\Controllers\Api\AwesomeCalcApiController;
 
-// We can remove the old polling approach for /api/appointments
-// or keep it if desired. Example below is commented out:
-
-/*
-use App\Models\Appointment;
-
-Route::get('/appointments', function () {
-    $appointments = Appointment::all();
-    return response()->json($appointments);
-});
-*/
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('/blogs', BlogApiController::class);
+// New API resource for storing/leasing calculators
+Route::apiResource('awesome', AwesomeCalcApiController::class);
