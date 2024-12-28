3e38c98bc79b138ea47fe6cbff9f8effc8c1b262
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 15:28:57 -0700

    Add file (plaintext)

---
+++ app/Models/AwesomeCalc.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AwesomeCalc extends Model
+{
+    use HasFactory;
+
+    protected $table = 'awesome_calcs';
+
+    protected $fillable = [
+        'calc_type',
+        'vehicle_price',
+        'rebates_and_discounts',
+        'down_payment',
+        'term_months',
+        'residual_percent',
+        'residual_value',
+        'money_factor',
+        'tax_percent',
+        'tax_total',
+        'capitalize_taxes',
+        'additional_fees',
+        'capitalize_fees',
+        'maintenance_cost',
+        'monthly_insurance',
+        'monthly_fuel',
+    ];
+}
