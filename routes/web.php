5614636900c787b97ecfee9fa1111b55c7ff4d57
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 17:48:53 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AwesomeCalcController;
 
-// We keep the existing routes but add a new ?awesome? page route:
-Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])->name('awesome.spa');
-
-// You might have an API route for actual CRUD in routes/api.php, but if we do it here:
-Route::prefix('awesome')->group(function () {
-    Route::get('/{id}', [AwesomeCalcController::class, 'show'])->name('awesome.show');
-    // The SPA might load an individual record in the form
-});
+/**
+ * Basic route to serve the SPA blade at /awesome.
+ * We won't do resource routes here, let's keep the full CRUD in /api/awesome
+ */
+Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])
+     ->name('awesome.spa');
