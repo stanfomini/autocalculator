2d71b8c4027f87f0cdc12da718cc34dd1002736e
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 18:05:29 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,8 @@ use App\Http\Controllers\AwesomeCalcController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| A single route to serve our SPA at /awesome
+| A single route for our SPA at /awesome
 |
 */
 
-Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])
-     ->name('awesome.spa');
+Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])->name('awesome.spa');
