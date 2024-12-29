20d13a16868e2001933cfb3a9b6677e286844209
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-29 12:00:53 -0700

    Update from ChatGPT (plaintext)

--- resources/views/awesome/show.blade.php
+++ resources/views/awesome/show.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Show Calculator</title>
 </head>
-<body style="background-color: #1e1e2f; color:#eee; font-family:sans-serif;">
+<body style="background-color:#1e1e2f; color:#eee; font-family:sans-serif;">
   <h1>AwesomeCalc #{{ $calc->id }}</h1>
   <p>Type: {{ $calc->calc_type }}</p>
   <p>Vehicle Price: {{ $calc->vehicle_price }}</p>
