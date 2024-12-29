1c71cea382d32a3a42a3d90b1e3b2a51131a7968
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 17:48:57 -0700

    Update from ChatGPT (plaintext)

--- app/Http/Controllers/AwesomeCalcController.php
+++ app/Http/Controllers/AwesomeCalcController.php
@@ -3,22 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\AwesomeCalc;
 
 class AwesomeCalcController extends Controller
 {
-    // Return a new SPA-like Blade that has tabs:
-    // 1) Calculator form
-    // 2) List of existing calculators
+    // Return the single-page Blade view at /awesome
     public function spaIndex()
     {
         return view('awesome.index');
-    }
-
-    // Fallback if user visits /awesome/{id} in a browser, show minimal details or redirect
-    public function show(AwesomeCalc $awesome)
-    {
-        // Could just return a blade with the data or redirect to the SPA
-        return view('awesome.show', ['calc' => $awesome]);
     }
 }
