2b03f7d4c347ff1cf0180a411d1bbb2c1c362cbc
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 17:58:23 -0700

    Update from ChatGPT (plaintext)

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AwesomeCalcApiController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Our CRUD routes for the "awesome" calculators.
+|
+*/
+
 Route::apiResource('awesome', AwesomeCalcApiController::class);
