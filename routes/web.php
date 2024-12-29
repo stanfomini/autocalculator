5590881c7e2b4200b18deec3c2cd04ae61e1b81d
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 17:58:22 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AwesomeCalcController;
 
-/**
- * Basic route to serve the SPA blade at /awesome.
- * We won't do resource routes here, let's keep the full CRUD in /api/awesome
- */
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| A single route to serve our SPA at /awesome
+|
+*/
+
 Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])
      ->name('awesome.spa');
