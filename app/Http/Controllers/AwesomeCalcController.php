1b6e59448ba08c90deaa98be117bbdad0e533bb6
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-29 12:26:28 -0700

    Update from ChatGPT (plaintext)

--- app/Http/Controllers/AwesomeCalcController.php
+++ app/Http/Controllers/AwesomeCalcController.php
@@ -8,7 +8,6 @@ class AwesomeCalcController extends Controller
 {
     public function spaIndex()
     {
-        // Return the Blade with the old calculator + new "Saved" page
         return view('awesome.index');
     }
 }
