a3d22f71b415c5ed0fbc534b03aebccbef11a9d5
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-28 18:43:39 -0700

    Update from ChatGPT (plaintext)

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AwesomeCalcController;
 
-Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])->name('awesome.spa');
+Route::get('/awesome', [AwesomeCalcController::class, 'spaIndex'])
+     ->name('awesome.spa');
