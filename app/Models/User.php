0a3b2fa18be9715e5f48386d8ca8aed275b291e3
Matthew <38113106+stanfomini@users.noreply.github.com>
2024-12-30 11:07:28 -0700

    Update from ChatGPT (plaintext)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,80 +2,35 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// typical user model after installing fortify, includes traits
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
+use App\Models\AwesomeCalc;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
-    use HasFactory;
-    use HasProfilePhoto;
-    use HasTeams;
-    use Notifiable;
-    use TwoFactorAuthenticatable;
-    use HasRoles;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasApiTokens, HasProfilePhoto, Notifiable, TwoFactorAuthenticatable;
+
     protected $fillable = [
         'name',
         'email',
-	'password',
-	'bio',
+        'password',
+        // etc.
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
+        // etc.
     ];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
-    protected $appends = [
-        'profile_photo_url',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    // each user can have many calculators
+    public function awesomeCalcs()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(AwesomeCalc::class);
     }
-
-    public function assignments()
-{
-    return $this->hasMany(Assignment::class);
-}
-
-    public function messages()
-{
-    return $this->morphMany(Message::class, 'sender');
-}
-
-
 }
